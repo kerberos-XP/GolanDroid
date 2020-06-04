deb973301767b47c1bf9240a021dfd3712337bdd
Omar Paché <omar.pache@gmail.com>
2020-06-03 23:36:54 -0400

    Mostrando los CB agregados en una ListView

--- GolanCapturator/app/src/main/java/cl/golan/golancapturador/MainActivity.kt
+++ GolanCapturator/app/src/main/java/cl/golan/golancapturador/MainActivity.kt
@@ -3,6 +3,7 @@ package cl.golan.golancapturador
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.Gravity
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -22,15 +23,40 @@ class MainActivity : AppCompatActivity() {
          */
         btnAgregar.setOnClickListener {
 
+            // Validar ingreso de cantidad
+            if (txtCantidad.text.toString().isEmpty() || txtCantidad.text.toString().equals("0")) {
+                var alerta = AlertDialog.Builder(this)
+                alerta.setCancelable(true)
+                alerta.setTitle("Aviso")
+                alerta.setMessage("Ingrese una cantidad mayor que 0 y menor que 1000000.")
+                alerta.show()
+                return@setOnClickListener
+            }
+
+            // Validar ingreso de código de barra
+            if (txtCodigoBarra.text.isEmpty() || txtCodigoBarra.text.toString().length < 6) {
+                var alerta = AlertDialog.Builder(this)
+                alerta.setCancelable(true)
+                alerta.setTitle("Aviso")
+                alerta.setMessage("Ingrese un código de barra de a lo menos 6 dígitos.")
+                alerta.show()
+                return@setOnClickListener
+            }
+
             var codigoBarra = txtCodigoBarra.text.toString()
             datos.add(codigoBarra)
 
+            val adaptador = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datos)
+            lista.adapter = adaptador
 
             txtCodigoBarra.setText("")
             txtCodigoBarra.requestFocus()
 
-            Toast.makeText(this, "Se agregó: " + codigoBarra + " x 1",
-                Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this,
+                "Se agregó: " + codigoBarra + " x " + txtCantidad.text.toString(),
+                Toast.LENGTH_SHORT
+            ).show()
         }
 
         /**
@@ -40,6 +66,7 @@ class MainActivity : AppCompatActivity() {
             // Crear hilo que contenga la lógica, así  no se pega la app
 
             txtCodigoBarra.setText("")
+            txtCantidad.setText("1")
             txtCodigoBarra.requestFocus()
 
             var alerta = AlertDialog.Builder(this)
